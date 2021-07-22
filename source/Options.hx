6c3152e46ed0dc2c5d27701a9eaccf4bfcafa7a6
Epic Riff <86388177+Epic-Riff@users.noreply.github.com>
2021-07-22 14:46:36 -0400

    Add files via upload

---
+++ source/Options.hx
@@ -0,0 +1,146 @@
+package;
+
+import flixel.util.FlxColor;
+import Controls.KeyboardScheme;
+import flixel.FlxG;
+import openfl.display.FPS;
+import openfl.Lib;
+
+class OptionCatagory
+{
+	private var _options:Array<Option> = new Array<Option>();
+	public final function getOptions():Array<Option>
+	{
+		return _options;
+	}
+
+	public final function addOption(opt:Option)
+	{
+		_options.push(opt);
+	}
+
+	
+	public final function removeOption(opt:Option)
+	{
+		_options.remove(opt);
+	}
+
+	private var _name:String = "New Catagory";
+	public final function getName() {
+		return _name;
+	}
+
+	public function new (catName:String, options:Array<Option>)
+	{
+		_name = catName;
+		_options = options;
+	}
+}
+
+class Option
+{
+	public function new()
+	{
+		display = updateDisplay();
+	}
+	private var description:String = "";
+	private var display:String;
+	public final function getDisplay():String
+	{
+		return display;
+	}
+
+	public final function getDescription():String
+	{
+		return description;
+	}
+
+	// Returns whether the label is to be updated.
+	public function press():Bool { return throw "stub!"; }
+	private function updateDisplay():String { return throw "stub!"; }
+}
+
+class DFJKOption extends Option
+{
+	private var controls:Controls;
+
+	public function new(controls:Controls)
+	{
+		super();
+		this.controls = controls;
+	}
+
+	public override function press():Bool
+	{
+		OptionsMenu.instance.openSubState(new KeyBindMenu());
+		return false;
+	}
+
+	private override function updateDisplay():String
+	{
+		return "Key Bindings";
+	}
+}
+
+
+class DownscrollOption extends Option
+{
+	public function new(desc:String)
+	{
+		super();
+		description = desc;
+	}
+
+	public override function press():Bool
+	{
+		FlxG.save.data.downscroll = !FlxG.save.data.downscroll;
+		display = updateDisplay();
+		return true;
+	}
+
+	private override function updateDisplay():String
+	{
+		return FlxG.save.data.downscroll ? "Downscroll" : "Upscroll";
+	}
+}
+
+
+class SongPositionOption extends Option
+{
+	public function new(desc:String)
+	{
+		super();
+		description = desc;
+	}
+	public override function press():Bool
+	{
+		FlxG.save.data.songPosition = !FlxG.save.data.songPosition;
+		display = updateDisplay();
+		return true;
+	}
+
+	private override function updateDisplay():String
+	{
+		return "Song Position " + (!FlxG.save.data.songPosition ? "off" : "on");
+	}
+}
+
+class ScrollSpeedOption extends Option
+{
+	public function new(desc:String)
+	{
+		super();
+		description = desc;
+	}
+
+	public override function press():Bool
+	{
+		return false;
+	}
+
+	private override function updateDisplay():String
+	{
+		return "Scroll Speed";
+	}
+}
+
